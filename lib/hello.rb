c775f152498855b47ee881e0f929b00d1bb1c434
Thanh Pham <tpham0123@gmail.com>
2020-05-13 16:29:43 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,7 +1,9 @@
-name = ["Tim", "Tom", "Jim"]
+people = ["Tim", "Tom", "Jim"]
 def hello_t
-  []
-
+  [people].each do |name|
+    if name.start_with?("T")
+      puts "Hi, 3{name}"
+    end
 end
 
 # call your method here!
